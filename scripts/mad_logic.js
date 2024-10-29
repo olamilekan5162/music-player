af323c5ce3bf210a9c5821312cc2308596c8338c
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-10-29 18:50:25 +0100

    now im having problem with cors

--- scripts/mad_logic.js
+++ scripts/mad_logic.js
@@ -1,5 +1,8 @@
+const mainMain = document.querySelector('.main-main')
+console.log(mainMain)
+const MY_API = 'https://api.deezer.com/search?q=pop'
+
 async function fetchMusic(){
-    const MY_API = 'https://api.deezer.com/search?q=pop'
     try{
 
         const res = await fetch(MY_API)
@@ -24,11 +27,26 @@ function displayMusic(data){
     data.forEach((music) => {
         // learnt this and i felt it is cool than adding child element one by one
 
-        trackContainer = `
+        const tracksContainer = `
         
-        `
-        console.log(music.artist.name)
+        <div class="track-box">
+
+                        <div class="track-image">
+                            <img src="${music.artist.picture_medium}" id="cover-art">
+
+                        </div>
+
+                        <p id="track-artist">${music.artist.name}</p>
+
+                        <p id="track-title">${music.title}</p>
+
+                    </div>
+        `;
+
+        mainMain.insertAdjacentHTML('beforeend', tracksContainer)
+    //     console.log(music.artist.name)
+    //     console.log(music.artist.picture_medium)
+    //     console.log(music.title)
     });
    
 }
-
